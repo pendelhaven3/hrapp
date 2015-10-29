1679cdee7fda73a085adcb8294ffb09ee119ea35
PJ Miranda <pendelhaven3@gmail.com>
2015-10-29 19:42:31 +0800

    Fix ordering of employee list

--- src/main/java/com/pj/hrapp/dao/EmployeeRepository.java
+++ src/main/java/com/pj/hrapp/dao/EmployeeRepository.java
@@ -1,11 +1,13 @@
 package com.pj.hrapp.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.pj.hrapp.model.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
+	@Query("select e from Employee e order by e.nickname")
 	Employee findByEmployeeNumber(long employeeNumber);
 	
 }
