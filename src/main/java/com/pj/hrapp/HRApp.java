b4eb237f8a48018012d8c88103dd939155c37ede
PJ Miranda <pendelhaven3@gmail.com>
2015-11-12 21:19:47 +0800

    Handle database not found error

--- src/main/java/com/pj/hrapp/HRApp.java
+++ src/main/java/com/pj/hrapp/HRApp.java
@@ -1,11 +1,17 @@
 package com.pj.hrapp;
 
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 
 import com.pj.hrapp.controller.StageController;
+import com.pj.hrapp.gui.component.ShowDialog;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -25,6 +31,26 @@ public class HRApp extends Application {
 	public void start(Stage stage) throws Exception {
 		ConfigurableApplicationContext context = SpringApplication.run(HRApp.class, HRApp.args);
 		
+		if (isDatabaseNotFound(context)) {
+			ShowDialog.error("Database not found");
+		} else {
+			showMainMenuScreen(context, stage);
+		}
+	}
+
+	private boolean isDatabaseNotFound(ApplicationContext context) {
+		DataSource dataSource = context.getBean(DataSource.class);
+		
+		try {
+			dataSource.getConnection().close();
+		} catch (SQLException e) {
+			return true;
+		}
+		
+		return false;
+	}
+
+	private void showMainMenuScreen(ApplicationContext context, Stage stage) {
 		StageController stageController = context.getBean(StageController.class);
 		stageController.setStage(stage);
 		stageController.showMainMenuScreen();
