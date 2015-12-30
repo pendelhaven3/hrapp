4782e893d1efd86d4e9b8df41b1c10569cecbdea
PJ Miranda <pendelhaven3@gmail.com>
2015-12-30 14:50:16 +0800

    Quick hack for handling dec 25/26 non-working days

--- src/main/java/com/pj/hrapp/model/SemimonthlyPayslipBasicPayItem.java
+++ src/main/java/com/pj/hrapp/model/SemimonthlyPayslipBasicPayItem.java
@@ -2,12 +2,22 @@ package com.pj.hrapp.model;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import com.pj.hrapp.model.util.DateInterval;
 import com.pj.hrapp.util.DateUtil;
 
 public class SemimonthlyPayslipBasicPayItem extends PayslipBasicPayItem {
 
+	private static List<Date> nonWorkingDays = new ArrayList<>();
+	
+	public SemimonthlyPayslipBasicPayItem() {
+		nonWorkingDays.add(DateUtil.toDate("12/25/2015"));
+		nonWorkingDays.add(DateUtil.toDate("12/26/2015"));
+	}
+	
 	@Override
 	public BigDecimal getAmount() {
 		return rate.multiply(new BigDecimal(numberOfDays))
@@ -16,8 +26,8 @@ public class SemimonthlyPayslipBasicPayItem extends PayslipBasicPayItem {
 
 	private double getTotalNumberOfWorkingDays(DateInterval period) {
 		return (double)period.toDateList().stream()
-			.filter(date -> !DateUtil.isSunday(date))
+			.filter(date -> !DateUtil.isSunday(date) && !nonWorkingDays.contains(date))
 			.count();
 	}
-
+	
 }
