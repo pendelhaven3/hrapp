0928ed81809c8c47c3d700b1cfde4596314d2482
PJ Miranda <pendelhaven3@gmail.com>
2016-01-05 21:51:53 +0800

    Remove total amount column from payroll list table

--- src/main/java/com/pj/hrapp/service/impl/PayrollServiceImpl.java
+++ src/main/java/com/pj/hrapp/service/impl/PayrollServiceImpl.java
@@ -3,7 +3,6 @@ package com.pj.hrapp.service.impl;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.YearMonth;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -61,15 +60,7 @@ public class PayrollServiceImpl implements PayrollService {
 	
 	@Override
 	public List<Payroll> getAllPayroll() {
-		List<Payroll> payrolls = payrollDao.getAll();
-		for (Payroll payroll : payrolls) {
-			List<Payslip> payslips = new ArrayList<>();
-			for (Payslip payslip : payslipDao.findAllByPayroll(payroll)) {
-				payslips.add(getPayslip(payslip.getId()));
-			}
-			payroll.setPayslips(payslips);
-		}
-		return payrolls;
+		return payrollDao.getAll();
 	}
 
 	@Transactional
