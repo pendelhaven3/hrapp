cd0ae71808ef4c17c9d6ca80d7b2596c898abb2f
PJ Miranda <pendelhaven3@gmail.com>
2019-01-05 11:08:42 +0800

    Update version to 2.7.4

--- src/main/java/com/pj/hrapp/controller/StageController.java
+++ src/main/java/com/pj/hrapp/controller/StageController.java
@@ -104,7 +104,7 @@ public class StageController {
 	}
 	
 	public void setTitle(String title) {
-		stage.setTitle("HR App v2.7.3 - " + title);
+		stage.setTitle("HR App v2.7.4 - " + title);
 	}
 
 	public void showMainMenuScreen() {
