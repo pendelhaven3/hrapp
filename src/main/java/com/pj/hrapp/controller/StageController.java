137ee1c27c0ea04998abaa01301aef64f383242a
PJ Miranda <pendelhaven3@gmail.com>
2024-02-09 20:42:50 +0800

    Update version to 2.14.1

--- src/main/java/com/pj/hrapp/controller/StageController.java
+++ src/main/java/com/pj/hrapp/controller/StageController.java
@@ -106,7 +106,7 @@ public class StageController {
 	}
 	
 	public void setTitle(String title) {
-		stage.setTitle("HR App v2.14.0 - " + title);
+		stage.setTitle("HR App v2.14.1 - " + title);
 	}
 
 	public void showMainMenuScreen() {
